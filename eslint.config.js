baa3975206dcc410f5323828d42dd80fcd4d719f
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-04-15 07:50:50 +0200

    refactor: replace leanup stack-based lint/tooling config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,43 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const js = require('@eslint/js');
+const tsEslintPlugin = require('@typescript-eslint/eslint-plugin');
+const tsEslintParser = require('@typescript-eslint/parser');
 
 module.exports = [
   {
-    ignores: ['**/assets/**'],
+    ignores: ['**/assets/**', '**/dist/**', '**/node_modules/**'],
+    linterOptions: {
+      reportUnusedDisableDirectives: false,
+    },
   },
-  ...compat.config({
-    extends: ['./.eslintrc.js'],
-  }),
+  {
+    ...js.configs.recommended,
+    files: ['**/*.{js,jsx}'],
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsEslintParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+    },
+    rules: {
+      ...tsEslintPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
 ];
